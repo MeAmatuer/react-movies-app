230e9426e1fcd56c78252ea7a0487c344dcf840c
MeAmatuer <sandipan2602@gmail.com>
2019-08-23 06:35:26 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,18 @@ import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
+const TabContainer = function(props){
+    return(
+        <Typography component="div" style={{padding: 0}}>
+            {props.children}
+        </Typography>
+    );
+}
 
 class Header extends Component{
     constructor(){
@@ -36,11 +48,21 @@ class Header extends Component{
                     </div>
                 </header>
                 <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" 
-                onRequestClose={this.closeModalHandler}>
+                onRequestClose={this.closeModalHandler} className="login-modal">
                     <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login"/>
                         <Tab label="Register"/>
                     </Tabs>
+                    <TabContainer>
+                        <FormControl>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" type="text"/>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password"/>
+                        </FormControl>
+                    </TabContainer>
                 </Modal>
             </div>
         )
